9cbf0a08ad2138dcce4bd50a12fe696fecc03170
GarvGojariya <gsgojariya196003@gmail.com>
2024-07-26 09:45:19 +0530

    "Updated POST function in create-post route to use NextRequest instead of NextResponse, added error handling for invalid token"

--- src/app/api/post/create-post/route.ts
+++ src/app/api/post/create-post/route.ts
@@ -1,16 +1,16 @@
 import { cookies } from "next/headers";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "../../../../../utils/db";
 import * as jose from "jose"
 import { verifyToken } from "../../../../../utils/verifyToken";
 
-export async function POST(req: NextResponse) {
+export async function POST(req: NextRequest) {
     const token = await cookies().get("accessToken")
     const { url, caption } = await req.json()
     if (token) {
         try {
             const decodedToken = await verifyToken()
-            if (decodedToken.id) {
+            if (decodedToken && decodedToken.id) {
                 const post = await prisma.post.create({
                     data: {
                         url,
@@ -20,6 +20,12 @@ export async function POST(req: NextResponse) {
                 })
                 return NextResponse.json({
                     message: "Post created successfully",
+                })
+            } else {
+                return NextResponse.json({
+                    message: "Invalid token",
+                }, {
+                    status: 401
                 })
             }
         } catch (error) {
